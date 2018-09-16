f07c35bee7cdf1b464dcb9769d90ebdf892fa274
Allen Payne <Allen Payne@DESKTOP-88ABTDK>
2018-09-16 14:50:41 -0400

    Added some comments and to the README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Euler2
 My attempt at solving Project Euler #2
+I ended with the correct answer of 4613732.
--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -4,21 +4,21 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		long num1 = 1L;
 		long num2 = 2L;
-		long sum = num1 + num2;
+		long sum = num1 + num2; //calculate the next term in the sequence
 		long total = 2; // to count for the second term, which is even
 		ArrayList<Long> evens = new ArrayList<Long>();
-		while(num2 < 4000000) {
+		while(num2 < 4000000) { //only want terms before 4 million
 			if(sum%2 == 0) {
-				evens.add(sum);
+				evens.add(sum); // keeping track of all the even numbers
 			}
-			num1 = num2;
-			num2 = sum;
-			sum = num1 + num2;
+			num1 = num2; //first number is now the second
+			num2 = sum; // change the second number to the sum of the previous two
+			sum = num1 + num2; // calculate the new sum
 		}
 		for(long y : evens) {
-			total = total + y;
+			total = total + y; // add all of the evens together
 		}
-		System.out.println(total);
-		System.out.println(evens);
+		System.out.println(total); //ouputs the total
+		System.out.println(evens);// checking my array
 	}
 }
